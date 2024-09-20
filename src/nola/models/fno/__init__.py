c18144d2740f5bec70e4b44817d6cabb01e96343
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-09-20 15:30:38 +0200

    Add imports

--- src/nola/models/fno/__init__.py
+++ src/nola/models/fno/__init__.py
@@ -1,8 +1,11 @@
 """Utilities for interfacing with Fourier neural operators."""
 
 from . import dft
+from ._fixed_input_fno_block import FixedInputFNOBlock
+from ._fixed_input_fno_head import FixedInputFNOHead
 from ._fixed_input_spectral_convolution import (
     CongruenceTransform_FixedInputSpectralConvolution_Identity,
     FixedInputSpectralConvolution,
 )
+from ._fno_block import fno_block
 from ._spectral_convolution import spectral_convolution
