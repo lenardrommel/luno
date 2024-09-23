2b3cef48aa7e3569c5b93c929132119ea0db44bd
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-09-23 18:23:25 +0200

    Diagonal of outer product of `FNOLastLayerWeightJacobian`s

--- src/nola/jacobians/fno/_last_fno_block_weight_jacobian.py
+++ src/nola/jacobians/fno/_last_fno_block_weight_jacobian.py
@@ -4,8 +4,10 @@ import operator
 import jax
 from jax import numpy as jnp
 import linox
+from linox._arithmetic import CongruenceTransform
 
 from nola.models.fno import fno_block
+from nola.models.fno._periodic_interpolation import gridded_fourier_interpolation
 
 from collections.abc import Callable
 from jax.typing import ArrayLike, DTypeLike
@@ -21,6 +23,8 @@ class LastFNOBlockWeightJacobian(linox.LinearOperator):
         output_grid_shape: tuple[int, ...],
         projection: Callable[[jax.Array], jax.Array],
         num_output_channels: int,
+        z_in: ArrayLike | None = None,
+        v_out_0: ArrayLike | None = None,
         dtype: DTypeLike = jnp.single,
     ) -> None:
         self._v_in = jnp.asarray(v_in)
@@ -47,6 +51,10 @@ class LastFNOBlockWeightJacobian(linox.LinearOperator):
             signature=f"(n,k)->(m,k)",
         )
 
+        # Caches for intermediate values
+        self._z_in = z_in
+        self._v_out_0 = v_out_0
+
         super().__init__(
             shape=(
                 self.output_grid_size * self._num_output_channels,
@@ -56,24 +64,48 @@ class LastFNOBlockWeightJacobian(linox.LinearOperator):
         )
 
     @property
+    def v_in(self) -> jax.Array:
+        return self._v_in
+
+    @property
     def num_input_channels(self) -> int:
         return self._v_in.shape[-1]
 
     @property
-    def num_hidden_channels(self) -> int:
-        return self._R_0.shape[-2]
-
-    @property
-    def num_output_channels(self) -> int:
-        return self.num_output_channels
+    def R_0(self) -> jax.Array:
+        return self._R_0
 
     @property
     def num_modes(self) -> int:
         return self._R_0[..., 0, 0].size
 
     @property
+    def num_hidden_channels(self) -> int:
+        return self._R_0.shape[-2]
+
+    @property
+    def W_0(self) -> jax.Array:
+        return self._W_0
+
+    @property
+    def b_0(self) -> jax.Array:
+        return self._b_0
+
+    @property
+    def output_grid_shape(self) -> tuple[int, ...]:
+        return self._output_grid_shape
+
+    @property
     def output_grid_size(self) -> int:
         return functools.reduce(operator.mul, self._output_grid_shape)
+
+    @property
+    def projection(self) -> Callable[[jax.Array], jax.Array]:
+        return self._projection
+
+    @property
+    def num_output_channels(self) -> int:
+        return self.num_output_channels
 
     @functools.cached_property
     def _weights_0(self) -> jax.Array:
@@ -86,11 +118,22 @@ class LastFNOBlockWeightJacobian(linox.LinearOperator):
             )
         )
 
-    def _matmul(self, weights: jax.Array) -> jax.Array:
-        # Compute the Jacobian
-        J = self._vectorized_jvp(weights)
+    @property
+    def z_in(self) -> jax.Array:
+        if self._z_in is None:
+            self._compute_intermediates()
 
-        return J
+        return self._z_in
+
+    @property
+    def v_out_0(self) -> jax.Array:
+        if self._v_out_0 is None:
+            self._compute_intermediates()
+
+        return self._v_out_0
+
+    def _matmul(self, weights: jax.Array) -> jax.Array:
+        return self._vectorized_jvp(weights)
 
     def _last_fno_block_forward(self, weights: jax.Array) -> jax.Array:
         # Extract R, W, and b from weights
@@ -109,7 +152,7 @@ class LastFNOBlockWeightJacobian(linox.LinearOperator):
 
         b = b.reshape(self._b_0.shape)
 
-        v_out = fno_block(
+        v_out, _ = fno_block(
             self._v_in,
             R,
             W,
@@ -120,3 +163,88 @@ class LastFNOBlockWeightJacobian(linox.LinearOperator):
         y = self._projection(v_out)
 
         return y.reshape(-1)
+
+    def _compute_intermediates(self) -> None:
+        self._v_out_0, intermediates = fno_block(
+            self._v_in,
+            self._R_0,
+            self._W_0,
+            self._b_0,
+            self._output_grid_shape,
+        )
+
+        self._z_in = intermediates["spectral_convolution"]["z_in"]
+
+
+########################################################################################
+# Congruence Transforms ################################################################
+########################################################################################
+
+
+#############################################
+# (LastFNOBlockWeightJacobian, Identity) #
+#############################################
+
+
+class CongruenceTransform_LastFNOBlockWeightJacobian_Identity(CongruenceTransform):
+    @functools.cached_property
+    def projection_jacobian_outer_product_diagonal(self) -> jax.Array:
+        v_out_0 = self._A.v_out_0.reshape(-1, self._A.num_hidden_channels)
+
+        J = jax.vmap(
+            jax.jacobian(self._A.projection),
+            in_axes=0,
+            out_axes=0,
+        )(v_out_0)
+
+        return jnp.sum(J**2, axis=-1)
+
+
+@linox.congruence_transform.dispatch
+def _(
+    A: LastFNOBlockWeightJacobian, B: linox.Identity
+) -> CongruenceTransform_LastFNOBlockWeightJacobian_Identity:
+    return CongruenceTransform_LastFNOBlockWeightJacobian_Identity(A, B)
+
+
+@linox.diagonal.dispatch
+def _(JJT: CongruenceTransform_LastFNOBlockWeightJacobian_Identity) -> jax.Array:
+    J: LastFNOBlockWeightJacobian = JJT._A
+
+    # Spectral convolution
+    z = J.z_in
+    z_abs_sq = z.real**2 + z.imag**2
+
+    diag_sconv = jnp.sum(z_abs_sq[..., 0, :])
+
+    if any(m_out < m_in for m_out, m_in in zip(J.output_grid_shape[:-1], z.shape[:-2])):
+        raise NotImplementedError()
+
+    m = J.output_grid_shape[-1] // 2 + 1
+
+    if J.output_grid_shape[-1] % 2 == 0 and m <= z.shape[-2]:
+        diag_sconv += 4 * jnp.sum(z_abs_sq[..., 1 : min(m, z.shape[-2]) - 1, :])
+        diag_sconv += jnp.sum(z_abs_sq[..., m - 1, :])
+    else:
+        diag_sconv += 4 * jnp.sum(z_abs_sq[..., 1 : min(m, z.shape[-2]), :])
+
+    # Skip connection
+    v = J.v_in
+    v_interp = gridded_fourier_interpolation(
+        v,
+        axes=tuple(range(len(J.output_grid_shape))),
+        output_grid_shape=J.output_grid_shape,
+    )
+
+    diag_skip = jnp.sum(v_interp**2, axis=-1)
+    diag_skip = diag_skip.reshape(-1, 1)
+
+    # Bias
+    diag_bias = 1
+
+    # Projection
+    diag_proj = JJT.projection_jacobian_outer_product_diagonal
+
+    diag = diag_proj * (diag_sconv + diag_skip + diag_bias)
+
+    return diag.reshape(-1)
