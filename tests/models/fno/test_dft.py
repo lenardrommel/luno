b58e233331f84a0e45c91907f9fbc848ccea6094
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-09-26 12:40:13 +0200

    Update dft tests

--- tests/models/fno/test_dft.py
+++ tests/models/fno/test_dft.py
@@ -2,29 +2,50 @@ import jax
 from jax import numpy as jnp
 import numpy as np
 
+from pytest_cases import AUTO, parametrize
+
 import nola
 
 
-def test_rfftn_2d():
-    # Sample a random signal
-    key = jax.random.key(985367)
-    v = jax.random.normal(key, (16, 15, 3))
-
-    # Transform and truncate
-    num_modes = (3, 3)
+@parametrize(
+    "grid_shape,modes_shape",
+    (
+        ((3,), (2,)),
+        ((16, 15), (16, 3)),
+        ((16, 16), (6, 4)),
+        ((16, 32), (8, 8)),
+    ),
+    idgen=AUTO,
+)
+def test_rfftn_truncation(grid_shape: tuple[int, ...], modes_shape: tuple[int, ...]):
+    signal = jax.random.normal(
+        jax.random.key(985367 + sum(grid_shape) + sum(modes_shape)),
+        shape=grid_shape,
+    )
 
     z_trunc = nola.models.fno.dft.rfftn(
-        v,
-        modes_shape=(2 * num_modes[0], num_modes[1]),
-        axes=(0, 1),
+        signal,
+        modes_shape=modes_shape,
+        axes=tuple(range(signal.ndim)),
+        norm="forward",
     )
 
     # Reference implementation
-    z_ref = jnp.fft.rfftn(v, axes=(0, 1), norm="forward")
+    z = jnp.fft.rfftn(signal, axes=tuple(range(signal.ndim)), norm="forward")
 
-    z_trunc_ref = jnp.concatenate(
-        (z_ref[: num_modes[0], : num_modes[1]], z_ref[-num_modes[0] :, : num_modes[1]]),
-        axis=0,
+    z_trunc_ref = jnp.fft.fftshift(z, axes=tuple(range(z.ndim - 1)))
+    z_trunc_ref = z_trunc_ref[
+        *(
+            slice((n - m) // 2, n + (-(n - m) // 2))
+            for n, m in zip(z_trunc_ref.shape[:-1], modes_shape[:-1], strict=True)
+        ),
+        : modes_shape[-1],
+    ]
+    z_trunc_ref = jnp.fft.ifftshift(z_trunc_ref, axes=tuple(range(z.ndim - 1)))
+
+    np.testing.assert_allclose(
+        z_trunc,
+        z_trunc_ref,
+        rtol=1e-6,
+        atol=1e-7,
     )
-
-    np.testing.assert_allclose(z_trunc, z_trunc_ref, rtol=1e-5, atol=1e-5)
