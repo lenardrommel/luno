fda73593553df4a2144d10207dd76c608da40eac
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-09-28 00:08:04 +0200

    Fix imports

--- src/nola/models/fno/__init__.py
+++ src/nola/models/fno/__init__.py
@@ -3,4 +3,5 @@
 from . import dft
 from ._fft_grid import FFTGrid
 from ._fno_block import fno_block
+from ._periodic_interpolation import gridded_fourier_interpolation
 from ._spectral_convolution import spectral_convolution
