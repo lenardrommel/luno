b434005e4e6356d730802a22e46b8a0781ddb28b
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-09-28 00:10:45 +0200

    Add `dtype` argument to `FFTGrid`

--- src/nola/models/fno/_fft_grid.py
+++ src/nola/models/fno/_fft_grid.py
@@ -1,9 +1,13 @@
 import numpy as np
 
+from numpy.typing import DTypeLike
+
 
 class FFTGrid(np.ndarray):
-    def __new__(cls, shape: tuple[int, ...]):
-        grids = tuple(np.linspace(0.0, 1.0, n, endpoint=False) for n in shape)
+    def __new__(cls, shape: tuple[int, ...], dtype: DTypeLike = np.double):
+        grids = tuple(
+            np.linspace(0.0, 1.0, n, endpoint=False, dtype=dtype) for n in shape
+        )
 
         obj = np.stack(
             np.meshgrid(
