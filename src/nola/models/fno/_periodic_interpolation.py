61c9642a2c3acf161cd2c2561c9201f6234d2795
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-09-28 00:07:26 +0200

    Refactor `gridded_fourier_interpolation`

--- src/nola/models/fno/_periodic_interpolation.py
+++ src/nola/models/fno/_periodic_interpolation.py
@@ -1,11 +1,22 @@
+from jax import numpy as jnp
+
+from jax.typing import ArrayLike
+
 from . import dft
 
 
 def gridded_fourier_interpolation(
-    v,
+    v: ArrayLike,
     axes: tuple[int, ...],
     output_grid_shape: tuple[int, ...],
 ):
+    v = jnp.asarray(v)
+
+    input_grid_shape = tuple(v.shape[axis] for axis in axes)
+
+    if output_grid_shape == input_grid_shape:
+        return v
+
     v_rdft = dft.rfftn(
         v,
         axes=axes,
@@ -16,6 +27,7 @@ def gridded_fourier_interpolation(
         v_rdft,
         grid_shape=output_grid_shape,
         axes=axes,
+        last_axis_from_even=input_grid_shape[-1] % 2 == 0,
         norm="forward",
     )
 
